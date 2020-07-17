db0ffb19a748513edfaa46c226875800daaa8d0f
Servio Zambrano <servio.za@gmail.com>
2020-07-17 19:59:25 -0400

    c

--- src/page/User/index.js
+++ src/page/User/index.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Spinner } from "react-bootstrap";
 import BasicLayout from "../../layout/BasicLayout";
+import { withRouter } from "react-router-dom";
+import { getUserApi } from "../../api/user";
+import { toast } from "react-toastify";
 
 import "./index.scss";
 
-export default function User() {
+function User(props) {
+  const { match } = props;
+  const [user, setUser] = useState(null);
+  const { params } = match;
+
+  useEffect(() => {
+    getUserApi(params.id)
+      .then((response) => {
+        setUser(response);
+        if (!response) {
+          toast.error("El usuario que haz visitado no existe");
+        }
+      })
+      .catch(() => {
+        toast.error("El usuario que haz visitado no existe");
+      });
+  }, [params]);
+
   return (
     <BasicLayout className="user">
       <div className="user__title">
-        <h2>Servio</h2>
+        <h2>
+          {user ? `${user.nombre} ${user.apellido}` : "Usuario no existe"}
+        </h2>
       </div>
       <div>Banner de usuario</div>
       <div>Info Usuario</div>
@@ -16,3 +38,5 @@ export default function User() {
     </BasicLayout>
   );
 }
+
+export default withRouter(User);
